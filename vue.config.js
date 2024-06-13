53eee4308040872576d324f8d1ec4c97b932e897
Nicole-Hf <nic.hf27@gmail.com>
2024-06-13 15:27:21 -0400

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    outputDir: 'dist',
-}
+  devServer: {
+    proxy: {
+      "/graphql": {
+        target: "http://18.218.15.90:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+};
