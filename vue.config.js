fb6daf5fa07f4ba8b5cdd0d75ef44f79b43f47fa
Nicole-Hf <nic.hf27@gmail.com>
2024-06-13 15:33:17 -0400

    jjj

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   devServer: {
     proxy: {
-      "/graphql": {
-        target: "http://18.218.15.90:8080",
+      "/": {
+        target: "http://18.218.15.90:8080", // URL HTTP de tu backend
         changeOrigin: true,
+        pathRewrite: { "^/": "" },
         secure: false,
       },
     },
